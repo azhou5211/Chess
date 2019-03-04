58cb5c8111ad49fa22ab930becf9a9288903837b
azhou5211 <32046010+azhou5211@users.noreply.github.com>
2019-03-04 13:33:08 -0500

    testing Chess.java

---
+++ src/chess/Chess.java
@@ -0,0 +1,16 @@
+package chess;
+	
+/**
+ * 
+ * @author Andrew Zhou, Bang An
+ * JSON external library has been used
+ */
+
+
+public class Chess {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
